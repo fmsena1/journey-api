963f7e5e687cf3a17969c09313b782342b0ad376
Filipe Magalhães Sena <94627887+fmsena1@users.noreply.github.com>
2024-08-04 18:58:26 -0300

    chore: initial commit

---
+++ Journey/src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,57 @@
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.GetById;
+using Journey.Application.UseCases.Trips.Resgiter;
+using Journey.Communication.Requests;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            try
+            {
+                var useCase = new RegisterTripUseCase();
+
+                var response = useCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch (JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ResourceErrorMessages.UNKNOWN_ERROR);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var useCase = new GetAllTripsUseCase();
+
+            var result = useCase.Execute();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var useCase = new GetByIdUseCase();
+
+            var result = useCase.Execute(id);
+
+            return Ok(result);
+        }
+    }
+}
